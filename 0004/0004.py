04ad628b70e1c2a88e08c7912068de6e23152151
tianshixu <tonyxu0124@yahoo.com>
2017-09-26 22:05:55 +0800

    Add 0004

---
+++ 0004/0004.py
@@ -0,0 +1,22 @@
+"""
+第 0004 题： 任一个英文的纯文本文件，统计其中的单词出现的个数。
+"""
+import re
+
+
+def findword(data):
+    words = re.compile('[a-zA-Z0-9]+')
+    dict = {}
+    for word in words.findall(data):
+        if word not in dict:
+            dict[word] = 1
+        else:
+            dict[word] += 1
+    return dict
+
+if __name__ == "__main__":
+    with open('./sample','r') as file:
+        data = file.read()
+        wordlist = findword(data)
+        print(wordlist)
+        file.close()
