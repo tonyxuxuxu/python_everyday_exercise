22b0c0b6cb4c9131eaf539dbeb553452039ddf52
tianshixu <tonyxu0124@yahoo.com>
2017-10-01 11:38:04 +0800

    Add modify

--- 0008/0008.py
+++ 0008/0008.py
@@ -9,11 +9,8 @@ from bs4 import BeautifulSoup
 def searchhtml(path):
     with open(path, 'r', encoding='utf-8') as readhtml:
         text = BeautifulSoup(readhtml, 'lxml')
-        urls = text.findAll('a')
-        for u in urls:
-            print(u['href'])
         content = text.get_text().split('\n')
-    return content
+    print(content)
 
 if __name__ == "__main__":
     searchhtml("./0008.html")
